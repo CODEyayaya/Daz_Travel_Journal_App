5aafb4e1d8d2ebee1ba200fb72ee301b74edd33f
CODEyayaya <divishabalaraju@gmail.com>
2026-07-20 13:42:15 +0530

    added and aadjusted globe, fixed centering

---
+++ app.js
@@ -0,0 +1,7 @@
+const globe = Globe()
+  (document.getElementById('globe-container'))
+  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
+  .backgroundColor('rgba(0,0,0,0)')
+  .width(460)
+  .height(460)
+  .pointOfView({ altitude: 2.2 });
